0fa9c2b882983c29c3db00e17eee629707e6dcf4
Jessika Bryant <bryantj2@oregonstate.edu>
2024-05-07 12:46:16 -0700

    Added a new basic pipeline for the course

---
+++ AnalysisPipeline.py
@@ -0,0 +1,15 @@
+###Pipeline to assemble genomes, infer gene trees and genome trees to investigate phylogenetic incongruence.
+#load modules
+import pandas as pd
+from os import sys
+
+
+###Input Gene family sequences
+
+
+
+###Perform Multiple Sequence Alignment (Mafft)
+
+###Infer Phylogeny of Gene Tree (IQ Tree)
+
+###Output a visual Gene Tree (Biopython)
